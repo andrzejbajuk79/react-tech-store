8931bbd8d2f130fefefa4e3dc3826b4b4647b5b3
andrzejbajuk79 <ptasiek3003@gmail.com>
2020-10-14 19:38:30 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import {FaHome} from 'react-icons/fa';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App () {
-  return <h1> hello from app</h1>;
+  return <h1> hello from app11 <FaHome /> </h1>;
 }
 
 export default App;
